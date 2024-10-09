4dc579eaf70d5d55f86951d459bdbe81df76c8a7
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-09 18:17:52 +0100

    Add thank-you/ page

---
+++ src/app/thank-you/page.jsx
@@ -0,0 +1,27 @@
+import Header from "@/components/header";
+import { Button } from "@/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <>
+      <Header hideLinks />
+      <div className="container max-h-screen mx-auto flex flex-col items-center">
+        <Image
+          src="/images/thank-you.png"
+          alt="An image of a Thank You Confetti"
+          width={700}
+          height={700}
+          className="w-[450px]"
+        />
+        <Button className="-mt-8" asChild>
+          <Link href="/">Back to Homepage</Link>
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ThankYou;
