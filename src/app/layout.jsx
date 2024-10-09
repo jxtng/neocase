2ac60f6b52d9d4cf62c6d0a07df8357e762fa103
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-09 12:53:31 +0100

    Initial setup + shadcn-ui

---
+++ src/app/layout.jsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Neocase",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
